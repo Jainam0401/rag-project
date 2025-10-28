65d9fff863ebb408572902524e8dd880f9089fad
Jainam <jainammithalaljain@gmail.com>
2025-10-28 10:59:08 +0530

    corrected cors error

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const apiClient = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "/",
   headers: {
     'Content-Type': 'application/json',
   },
